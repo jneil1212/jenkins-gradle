e2f8ee32cdc6cbbf94979d48772d15a9b7bc3502
req89599 <Mu09Om17>
2018-02-06 10:06:11 -0600

    Initial

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export WORKSPACE=`pwd`
+
+# Create/Activate virtualenv
+virtualenv venv
+source venv/bin/activate
+
+# Install Requirements
+pip install -r requirements.txt
+
+# Run tests
+python manage.py test
+
+# Deactivate virtualenv
+deactivate
+
